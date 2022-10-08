8a6a24274bcc43498fbe142b0e11397c3ba7e9be
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2022-10-07 20:44:48 -0500

    agrega vistas html

---
+++ src/main/java/com/example/demospringrest/controllers/AppController.java
@@ -0,0 +1,28 @@
+package com.example.demospringrest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+	@GetMapping("/")
+	public String root(Model model) {
+		return "home";
+	}
+
+	@GetMapping("/home")
+	public String home(Model model) {
+		return "home";
+	}
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		return "hello";
+	}
+
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
+}
