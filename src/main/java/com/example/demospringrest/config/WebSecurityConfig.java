fe912069efcbaebf77fdc75c7d8791b2fab55a56
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2022-10-12 16:54:25 -0500

    agrega implementacion de AuthenticationManager

--- src/main/java/com/example/demospringrest/config/WebSecurityConfig.java
+++ src/main/java/com/example/demospringrest/config/WebSecurityConfig.java
@@ -3,11 +3,13 @@ package com.example.demospringrest.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -39,15 +41,17 @@ public class WebSecurityConfig {
 
 		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 
-		http.userDetailsService(userService);
-
 		return http.build();
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration)
-			throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
+	public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+			PasswordEncoder passwordEncoder) {
+		var authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+		return new ProviderManager(authenticationProvider);
 	}
 
 	@Bean
--- src/main/java/com/example/demospringrest/services/AuthService.java
+++ src/main/java/com/example/demospringrest/services/AuthService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.example.demospringrest.dtos.LoginRequest;
@@ -32,8 +31,6 @@ public class AuthService {
 
 		Authentication authentication = authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
-
-		SecurityContextHolder.getContext().setAuthentication(authentication);
 
 		LoginResponse loginResponse = new LoginResponse();
 
