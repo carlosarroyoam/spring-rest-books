00268571424c004c92c50cae310f82dacf8609f1
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2024-03-09 15:39:01 -0600

    Agrega ExceptionHandlers

--- src/main/java/com/carlosarroyoam/rest/books/exception/ControllerAdvisor.java
+++ src/main/java/com/carlosarroyoam/rest/books/exception/ControllerAdvisor.java
@@ -8,11 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.carlosarroyoam.rest.books.dto.AppExceptionResponse;
 
@@ -32,6 +35,51 @@ class ControllerAdvisor {
 		appExceptionResponse.setTimestamp(ZonedDateTime.now(ZoneId.of("UTC")));
 
 		return new ResponseEntity<>(appExceptionResponse, ex.getStatusCode());
+	}
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<AppExceptionResponse> handleNoHandlerFoundException(NoHandlerFoundException ex,
+			WebRequest request) {
+		HttpStatus statusCode = HttpStatus.NOT_FOUND;
+
+		AppExceptionResponse appExceptionResponse = new AppExceptionResponse();
+		appExceptionResponse.setMessage("No endpoint found for: " + request.getDescription(false).replace("uri=", ""));
+		appExceptionResponse.setError(statusCode.getReasonPhrase());
+		appExceptionResponse.setStatus(statusCode.value());
+		appExceptionResponse.setPath(request.getDescription(false).replace("uri=", ""));
+		appExceptionResponse.setTimestamp(ZonedDateTime.now(ZoneId.of("UTC")));
+
+		return new ResponseEntity<>(appExceptionResponse, statusCode);
+	}
+
+	@ExceptionHandler(NoResourceFoundException.class)
+	public ResponseEntity<AppExceptionResponse> handleNoResourceFoundException(NoResourceFoundException ex,
+			WebRequest request) {
+		HttpStatus statusCode = HttpStatus.NOT_FOUND;
+
+		AppExceptionResponse appExceptionResponse = new AppExceptionResponse();
+		appExceptionResponse.setMessage("No static resource: " + request.getDescription(false).replace("uri=", ""));
+		appExceptionResponse.setError(statusCode.getReasonPhrase());
+		appExceptionResponse.setStatus(statusCode.value());
+		appExceptionResponse.setPath(request.getDescription(false).replace("uri=", ""));
+		appExceptionResponse.setTimestamp(ZonedDateTime.now(ZoneId.of("UTC")));
+
+		return new ResponseEntity<>(appExceptionResponse, statusCode);
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<AppExceptionResponse> handleHttpRequestMethodNotSupportedException(
+			HttpRequestMethodNotSupportedException ex, WebRequest request) {
+		HttpStatus statusCode = HttpStatus.BAD_REQUEST;
+
+		AppExceptionResponse appExceptionResponse = new AppExceptionResponse();
+		appExceptionResponse.setMessage(ex.getMessage());
+		appExceptionResponse.setError(statusCode.getReasonPhrase());
+		appExceptionResponse.setStatus(statusCode.value());
+		appExceptionResponse.setPath(request.getDescription(false).replace("uri=", ""));
+		appExceptionResponse.setTimestamp(ZonedDateTime.now(ZoneId.of("UTC")));
+
+		return new ResponseEntity<>(appExceptionResponse, statusCode);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
