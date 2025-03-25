ee6e62e7030915293bb61358c0e46d8828280707
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2025-03-25 11:27:56 -0600

    Elimina RoleDtoMapper en desuso

--- src/main/java/com/carlosarroyoam/rest/books/dto/RoleDto.java
+++ src/main/java/com/carlosarroyoam/rest/books/dto/RoleDto.java
@@ -1,14 +1,9 @@
 package com.carlosarroyoam.rest.books.dto;
 
-import com.carlosarroyoam.rest.books.entity.Role;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.factory.Mappers;
 
 @Data
 @NoArgsConstructor
@@ -18,13 +13,4 @@ public class RoleDto {
   private Integer id;
   private String title;
   private String description;
-
-  @Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-  public interface RoleDtoMapper {
-    RoleDtoMapper INSTANCE = Mappers.getMapper(RoleDtoMapper.class);
-
-    RoleDto toDto(Role role);
-
-    List<RoleDto> toDtos(List<Role> roles);
-  }
 }
