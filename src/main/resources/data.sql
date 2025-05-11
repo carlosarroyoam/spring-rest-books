52e2b9086e12e7331ed635546aa2eb7b82dfbd91
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2025-05-10 23:36:56 -0600

    Agrega scripts de sql para schema y datos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO authors(name, created_at, updated_at) VALUES ('Yuval Noah Harari', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO authors(name, created_at, updated_at) VALUES ('Itzik Yahav', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO books(isbn, title, cover_url, price, is_available_online, published_at, created_at, updated_at) 
+VALUES ('978-1-3035-0529-4', 'Homo Deus: A Brief History of Tomorrow', 'https://images.isbndb.com/covers/39/36/9781784703936.jpg', 22.99, 0, '2017-01-01', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO books(isbn, title, cover_url, price, is_available_online, published_at, created_at, updated_at) 
+VALUES ('978-9-7389-4434-3', 'Sapiens: A Brief History of Humankind', 'https://images.isbndb.com/covers/60/97/9780062316097.jpg', 20.79, 0, '2022-12-01', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO book_authors(book_id, author_id) VALUES (1, 1);
+INSERT INTO book_authors(book_id, author_id) VALUES (1, 2);
+INSERT INTO book_authors(book_id, author_id) VALUES (2, 1);
+
+INSERT INTO roles(description, title) VALUES ('Role for admins users', 'App//Admin');
+INSERT INTO roles(description, title) VALUES ('Role for customer users', 'App//Customer');
+
+INSERT INTO users(name, age, email, username, role_id, is_active, created_at, updated_at)
+VALUES ('Carlos Alberto Arroyo Martínez', 28, 'carroyom@mail.com', 'carroyom', 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO users(name, age, email, username, role_id, is_active, created_at, updated_at)
+VALUES ('Cathy Stefania Guido Rojas', 28, 'cguidor@mail.com', 'cguidor', 2, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE authors (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE books (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    isbn VARCHAR(17) NOT NULL UNIQUE,
+    title VARCHAR(128) NOT NULL,
+    cover_url VARCHAR(512) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    is_available_online BOOLEAN NOT NULL,
+    published_at DATE NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE book_authors (
+    book_id BIGINT NOT NULL,
+    author_id BIGINT NOT NULL,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (author_id) REFERENCES authors(id)
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(32) NOT NULL,
+    description VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    age TINYINT NOT NULL,
+    email VARCHAR(128) NOT NULL UNIQUE,
+    username VARCHAR(128) NOT NULL UNIQUE,
+    is_active BOOLEAN NOT NULL,
+    role_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
